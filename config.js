dddcc39879b8c7f271d7ac1639d48a088f0039ef
Roberto Simonetti <robisim74@gmail.com>
2021-05-28 18:31:02 +0200

    Cleanup

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ const config = {
      * Entry points as in src folder
      */
     entries: [
-        { name: 'home', template: './index.html', module: './index.ts', style: './index.scss', path: '/express-mvc' },
-        { name: 'about', template: './about/index.html', module: './about/index.ts', style: './about/index.scss', path: '/express-mvc/about' },
+        { name: 'home', template: './index.html', module: './index.ts', style: './index.scss', path: '' },
     ],
     /**
      * Files or directories to copy from src to the build directory
@@ -24,7 +23,7 @@ const config = {
     /**
      * For sitemap URLs
      */
-    hostname: 'https://robisim74.github.io'
+    hostname: 'https://example.com'
 };
 
 module.exports = config;
--- src/about/index.ts
+++
@@ -1,2 +0,0 @@
-/* Web Components */
-import '../components/nav-menu';
