53043cc360c7898726bdb27e1690b971e4df9c56
Roberto Simonetti <robisim74@gmail.com>
2021-12-10 18:28:11 +0100

    Update tailwind to v3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,10 @@ module.exports = {
 	/**
 	 * Whenever you compile your CSS with NODE_ENV set to production, Tailwind will automatically purge unused styles from your CSS
 	 */
-	purge: [
+	content: [
 		'./src/views/**/*.html'
 	],
-	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
-	},
-	variants: {
 		extend: {},
 	},
 	plugins: [],
