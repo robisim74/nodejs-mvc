56611fb01238875c5575ab4031dbc655cd0a819c
Roberto Simonetti <robisim74@gmail.com>
2021-12-10 18:33:54 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,4 @@
 module.exports = {
-	/**
-	 * Whenever you compile your CSS with NODE_ENV set to production, Tailwind will automatically purge unused styles from your CSS
-	 */
 	content: [
 		'./src/views/**/*.html'
 	],
