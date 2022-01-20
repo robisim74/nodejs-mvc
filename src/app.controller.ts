15f45efc654487c520e639cca5bbe3c1237dec84
Roberto Simonetti <robisim74@gmail.com>
2022-01-20 22:07:21 +0100

    CSP: add nonce sample

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,8 +4,9 @@ import { Request, Response } from 'express';
  * Index controller
  * @route GET /
  */
-export const index = (req: Request, res: Response) => {
+export const index = (req: Request, res: Response, nonce: string) => {
     res.render('index', {
-        name: 'Node.js MVC'
+        name: 'Node.js MVC',
+        nonce: nonce
     });
 };
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { engine } from 'express-handlebars';
 import helmet from 'helmet';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import crypto from 'crypto';
 
 import { index } from './app.controller';
 
@@ -11,6 +12,7 @@ const app: Application = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const environment = process.env.NODE_ENV;
 const port = process.env.PORT || 8080;
+const nonce = crypto.randomBytes(16).toString('base64');
 
 // Set the view engine to handlebars using html extension
 app.engine('html', engine(
@@ -26,7 +28,8 @@ app.use(
 	helmet.contentSecurityPolicy({
 		useDefaults: true,
 		directives: {
-			'connect-src': environment === 'production' ? ["'self'"] : ["'self' ws://localhost:3000"]
+			'connect-src': environment === 'production' ? ["'self'"] : ["'self'", "ws://localhost:3000"],
+			'script-src': ["'self'", `'nonce-${nonce}'`]
 		},
 	})
 );
@@ -34,7 +37,7 @@ app.use(
 /**
  * Primary app routes
  */
-app.get('/', index);
+app.get('/', (req, res) => index(req, res, nonce));
 
 app.listen(port, () => {
 	console.info(`Server listening on http://localhost:${port}`);
